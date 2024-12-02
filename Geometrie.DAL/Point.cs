82809a4f0f9bbfd8343bcd7cf6a0796ab318a7fa
Alan Ferronnière <ferronniere@hotmail.com>
2024-12-02 09:11:05 +0100

    Fin de cours 12/11/2024

--- Geometrie.DAL/Point.cs
+++ Geometrie.DAL/Point.cs
@@ -2,10 +2,28 @@
 {
     public class Point
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public int X { get; set; }
         public int Y { get; set; }
         public DateTime DateDeCreation { get; set; }
         public DateTime? DateDeModification { get; set; }
+
+        public Polygone? Polygone { get; set; }
+
+        public Point(int x, int y, DateTime dateDeCreation)
+        {
+            X = x;
+            Y = y;
+            DateDeCreation = dateDeCreation;
+        }
+
+        public Point(int id, int x, int y, DateTime dateDeCreation,
+                DateTime? dateDeModification, Polygone? polygone)
+            :this(x,y, dateDeCreation)
+        {
+            Id = id;
+            DateDeModification = dateDeModification;
+            Polygone = polygone;
+        }
     }
 }
