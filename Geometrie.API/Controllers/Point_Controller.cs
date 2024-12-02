c9637fcff5e483372c92e00697e7fa2f668437d3
Alan Ferronnière <ferronniere@hotmail.com>
2024-12-02 14:22:24 +0100

    encore une correction

--- Geometrie.API/Controllers/Point_Controller.cs
+++ Geometrie.API/Controllers/Point_Controller.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Geometrie.Service;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Geometrie.API.Controllers
 {
     public class Point_Controller : Controller
     {
-        
+        private Point_Service service;
+
+        public Point_Controller(Point_Service service)
+        {
+            this.service = service;
+        }
+
 
     }
 }
